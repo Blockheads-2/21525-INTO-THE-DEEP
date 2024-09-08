7994e81e73e6221a622ecabfc33e3296a60efe48
apricitine <apricitinecas@gmail.com>
2024-09-08 10:23:54 -0700

    add ftclib, init folders

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BaseDrive.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name="Base Drive", group="Beta")
+public class BaseDrive extends InheritableBasicTeleOp {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/LogFiles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/LogFiles.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode.util.roadrunner;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
