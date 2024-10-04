6fc38df045b76852d925d5734eae7d6cef395c67
apricitine <apricitinecas@gmail.com>
2024-10-03 18:19:38 -0700

    what

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Simulate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Simulate.java
@@ -5,14 +5,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.util.Utility;
 
 
-@TeleOp(name="Base Drive", group="beta")
+@TeleOp(name="Simulate", group="beta")
 public class Simulate extends InheritableTeleOp {
 
 
     @Override
     public void loop() {
-        drive(1);
-
-
+        if (gamepad1.a) {
+            robot.rightFront.setPower(0.5);
+            robot.leftFront.setPower(0.5);
+            robot.leftBack.setPower(0.5);
+            robot.rightBack.setPower(0.5);
+        } else {
+            robot.rightFront.setPower(0);
+            robot.leftFront.setPower(0);
+            robot.leftBack.setPower(0);
+            robot.rightBack.setPower(0);
+        }
     }
 }
